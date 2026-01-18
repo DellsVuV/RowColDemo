d87f53e44dbeba3d7027215bb80c9c5e3bd711d4
Compose <sonapogodaeva00017@gmail.com>
2026-01-19 03:40:18 +0700

    Viravnivanie5

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -39,37 +43,136 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Row {
-
-            Column {
-                TextCell("1")
-                TextCell("2")
-                TextCell("3")
-            }
-
-
-            Column {
-                TextCell("4")
-                TextCell("5")
-                TextCell("6")
-            }
-
-
-            Column {
-                TextCell("7")
-                TextCell("8")
-            }
-        }
-
-
-        Row {
-            TextCell("9")
-            TextCell("10")
-            TextCell("11")
-        }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Example4()
     }
 }
+
+
+@Composable
+fun Example4() {
+
+    Row(
+        modifier = Modifier
+            .size(width = 400.dp, height = 200.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example5() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .size(width = 400.dp, height = 200.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+@Composable
+fun Example6() {
+    Column(
+        horizontalAlignment = Alignment.End,
+        modifier = Modifier
+            .width(250.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example9() {
+    Row(
+        horizontalArrangement = Arrangement.End,
+        modifier = Modifier
+            .size(width = 400.dp, height = 200.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example10() {
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        modifier = Modifier
+            .height(400.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example11() {
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .width(1000.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example12() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .width(1000.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
+
+@Composable
+fun Example13() {
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = Modifier
+            .width(1000.dp)
+            .border(1.dp, Color.Gray)
+    ) {
+        TextCell("1")
+        TextCell("2")
+        TextCell("3")
+    }
+}
+
 
 @Composable
 fun TextCell(text: String, modifier: Modifier = Modifier) {
@@ -87,7 +190,8 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+// ▼▼▼ ПРЕВЬЮ ▼▼▼
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     RowColDemoTheme {
@@ -95,3 +199,11 @@ fun GreetingPreview() {
     }
 }
 
+
+@Preview
+@Composable
+fun Example4Preview() {
+    RowColDemoTheme {
+        Example4()
+    }
+}
